c26b9a1482345026a463d661171ed575a8d19859
Julino Segunda Dídimo <ddimojulino@gmail.com>
2020-09-27 22:15:48 -0300

    Adding...

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -37,9 +37,7 @@ function TeacherList() {
     }, [])
 
     function toogleIFilterVisible() {
-        return (
-            setFilter(!isFilterVisible)
-        )
+        setFilter(!isFilterVisible)
     }
 
     async function handleFilterSubmit() {
